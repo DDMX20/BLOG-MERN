d643df283ecd1bdbfc00c56f24ad3190387ca73a
Dhananjaya Dutt Mishra <dhananjayaduttmishra@gmail.com>
2020-07-26 16:20:13 +0530

    post and get  added

--- blogBackend/routes/postRoutes.js
+++ blogBackend/routes/postRoutes.js
@@ -1,7 +1,35 @@
 const router = require("express").Router();
+const Post = require("../models/postModel.js")
 
-router.get("/test",function (req,res) {
-res.send("its working")
+//Retrival of data from the database
+router.post("/",async (req,res) =>{
+
+  const {title,createdAt,tags,html}= req.body;
+// Creationm of Model
+  const newPost = new Post ({
+    title,createdAt,tags,html
+
+  });
+  // Saving data to the database
+  try
+  {
+     const savedPost = await newPost.save();
+    res.json(savedPost);
+  }catch(err){
+    console.error(err);
+  }
+});
+
+router.get("/",async (req,res)=>{
+  const posts = await Post.find();
+  res.json(posts)
 })
 
-module.exports =router;
+router.get("/:id",async (req,res)=>{
+  const post = await Post.findById(req.params.id);
+  res.json(post)
+})
+
+
+
+module.exports = router;
